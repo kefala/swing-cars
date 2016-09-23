3c4a60710db56beebe05f8238a101af024ecc895
Martín Lobo <kefala@MacBook-Pro-de-Martin.local>
2016-09-23 14:12:02 -0300

    Modeling

--- project/swing-cars/src/main/java/com/degueLobo/app/Entities/Autos/AutoDTO.java
+++ project/swing-cars/src/main/java/com/degueLobo/app/Entities/Autos/AutoDTO.java
@@ -1,6 +1,7 @@
 package com.degueLobo.app.Entities.Autos;
 
 import com.degueLobo.app.Entities.DTO;
+import com.degueLobo.app.Entities.Oficinas.OficinaDTO;
 
 /**
  * Created by kefala on 23/9/16.
@@ -8,4 +9,32 @@ import com.degueLobo.app.Entities.DTO;
 public class AutoDTO extends DTO{
     private AutoBaseDTO base;
     private String patente;
+    private OficinaDTO oficinaActual;
+
+    public AutoDTO() {
+    }
+
+    public AutoBaseDTO getBase() {
+        return base;
+    }
+
+    public void setBase(AutoBaseDTO base) {
+        this.base = base;
+    }
+
+    public String getPatente() {
+        return patente;
+    }
+
+    public void setPatente(String patente) {
+        this.patente = patente;
+    }
+
+    public OficinaDTO getOficinaActual() {
+        return oficinaActual;
+    }
+
+    public void setOficinaActual(OficinaDTO oficinaActual) {
+        this.oficinaActual = oficinaActual;
+    }
 }
