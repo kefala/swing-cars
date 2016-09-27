ada55f4dbd287b46804140465abcc330b3953daf
Kefala <martin.g.lobo@gmail.com>
2016-09-26 21:23:00 -0300

    Facha

--- project/swing-cars/src/main/java/com/degueLobo/app/App.java
+++ project/swing-cars/src/main/java/com/degueLobo/app/App.java
@@ -1,5 +1,8 @@
 package com.degueLobo.app;
 
+import com.degueLobo.app.Controllers.LoginController;
+import com.degueLobo.app.Models.LoginModel;
+import com.degueLobo.app.Views.LoginView;
 
 /**
  * Hello world!
@@ -9,11 +12,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        /*
         LoginModel model = new LoginModel();
         LoginView view = new LoginView(model);
         LoginController con = new LoginController(view, model);
         con.start();
-        */
     }
 }
